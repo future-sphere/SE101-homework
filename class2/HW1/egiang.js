f80f493b80428663af3297763518b9bb937d5a39
初旭 <chuxu200328@gmail.com>
2020-01-11 18:39:38 -0500

    class 3 homework

---
+++ class2/HW1/egiang.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("location");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  alert(
+    "Latitude: " +
+      position.coords.latitude +
+      "Longitude: " +
+      position.coords.longitude
+  );
+}
